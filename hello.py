e109f5d6c6cda0fb1560e1693c0324bf7512ca7d
Tanmay-jam <65532642+Tanmay-jam@users.noreply.github.com>
2020-08-13 11:52:59 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
+import urllib.request, urllib.parse, urllib.error
 print('Hello word')
 for i in range(0,3):
   print(i,i+1,i+3)
+fh = urllib.request.urlopen('https://www.w3.org/TR/PNG/iso_8859-1.txt')
+data = fh.read().decode()
+print(data)
